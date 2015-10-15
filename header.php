11b701c3e40cc990830ed5f7e2a79c604225e038
Jonathan Dueck <jon@collectif.co>
2015-10-15 10:43:13 -0500

    add favicons etc

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 
 	<!-- Always force latest IE rendering engine (even in intranet) -->
 	<!--[if IE ]>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<![endif]-->
 
 	<?php
@@ -31,7 +31,6 @@
 
 	<meta name="title" content="<?php wp_title( '|', true, 'right' ); ?>">
 
-	<!--Google will often use this as its description of your page/site. Make it good.-->
 	<meta name="description" content="<?php bloginfo('description'); ?>" />
 
 	<?php
@@ -44,40 +43,7 @@
 
 	<meta name="Copyright" content="Copyright &copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?>. All Rights Reserved.">
 
-	<?php
-		/*
-			j.mp/mobileviewport & davidbcalhoun.com/2010/viewport-metatag
-			 - device-width : Occupy full width of the screen in its current orientation
-			 - initial-scale = 1.0 retains dimensions instead of zooming out if page height > device height
-			 - maximum-scale = 1.0 retains dimensions instead of zooming in if page width < device width
-			 - minimal-ui = iOS devices have minimal browser ui by default
-		*/
-		if (true == of_get_option('meta_viewport'))
-			echo '<meta name="viewport" content="' . of_get_option("meta_viewport") . ' minimal-ui" />';
-
-
-		/*
-			These are for traditional favicons and Android home screens.
-			 - sizes: 1024x1024
-			 - transparency is OK
-			 - see wikipedia for info on browser support: http://mky.be/favicon/
-			 - See Google Developer docs for Android options. https://developers.google.com/chrome/mobile/docs/installtohomescreen
-		*/
-		if (true == of_get_option('head_favicon')) {
-			echo '<meta name=”mobile-web-app-capable” content=”yes”>';
-			echo '<link rel="shortcut icon" sizes=”1024x1024” href="' . of_get_option("head_favicon") . '" />';
-		}
-
-
-		/*
-			The is the icon for iOS Web Clip.
-			 - size: 57x57 for older iPhones, 72x72 for iPads, 114x114 for iPhone4 retina display (IMHO, just go ahead and use the biggest one)
-			 - To prevent iOS from applying its styles to the icon name it thusly: apple-touch-icon-precomposed.png
-			 - Transparency is not recommended (iOS will put a black BG behind the icon) -->';
-		*/
-		if (true == of_get_option('head_apple_touch_icon'))
-			echo '<link rel="apple-touch-icon" href="' . of_get_option("head_apple_touch_icon") . '">';
-	?>
+	<?php include "_includes/icons.php" ?>
 
 	<!-- concatenate and minify for production -->
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/reset.css" />
@@ -86,8 +52,7 @@
 	<!-- Lea Verou's Prefix Free, lets you use only un-prefixed properties in yuor CSS files -->
     <script src="<?php echo get_template_directory_uri(); ?>/_/js/prefixfree.min.js"></script>
 
-	<!-- This is an un-minified, complete version of Modernizr.
-		 Before you move to production, you should generate a custom build that only has the detects you need. -->
+	<!-- This is an un-minified, complete version of Modernizr. Before you move to production, you should generate a custom build that only has the detects you need. -->
 	<script src="<?php echo get_template_directory_uri(); ?>/_/js/modernizr-2.8.0.dev.js"></script>
 
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
@@ -106,7 +71,7 @@
 			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
 				<img src="<?php bloginfo('template_directory'); ?>/assets/img/shield.svg" alt="Co-op Logo" class="site-logo">
 			</a>
-			<!-- <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> -->
+
 			<div class="site-description"><?php bloginfo( 'description' ); ?><br><em><a href="<?php echo esc_url( home_url( '/beta/' ) ); ?>">βeta</a></em></div>
 
 			<nav id="nav" class="site-nav" role="navigation">
